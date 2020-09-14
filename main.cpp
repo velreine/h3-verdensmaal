8eb28c32cb6f71febdb37b9a3149fc83259776cb
Nicky Gottlieb Hansen <nicky-g-hansen@hotmail.com>
2020-09-14 13:30:54 +0200

    Opdateret...

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+//#define definer #define
+
+#define SANDT true
+#define FALSK false
+#define hvis if
+#define sandt true
+#define falsk false
+#define valgmulighed switch
+#define valg case
+#define stop break
+#define standard default
+#define tilbage return
+#define heltal int
+#define kommatal float
+#define tekst string
+#define orden array
+#define anvender using
+#define navnerum namespace
+#define udskriv cout
+#define indskriv cin
+#define slutlinje endl
+#define hovedmetode main
+#define mereind <<
+#define mereud >>
+#define semikolon ;
+#define kolon :
+#define tuborgvenstre {
+#define tuborghoejre }
+#define ethundrede 100
+#define erligmed ==
+#define parentesstart (
+#define parentesslut )
+
+anvender navnerum std;
+
+heltal hovedmetode parentesstart parentesslut tuborgvenstre
+
+  hvis parentesstart 1 erligmed SANDT parentesslut tuborgvenstre
+    udskriv mereind "Ja." mereind slutlinje semikolon
+  tuborghoejre
+
+  valgmulighed parentesstart ethundrede parentesslut
+  tuborgvenstre 
+    valg ethundrede kolon
+      udskriv mereind ethundrede mereind " hehe" mereind slutlinje semikolon
+      stop semikolon
+    standard kolon
+      stop semikolon
+  tuborghoejre
+
+  tilbage 0 semikolon
+tuborghoejre
